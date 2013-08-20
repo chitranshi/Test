3ae7c405fcf437ddf6c6f362ea202dce25d4a010
shubham chitranshi <shubham@hitachi.co.in>
2013-08-20 17:03:45 +0530

    foo2 added and foo edited

--- foo.java
+++ foo.java
@@ -3,6 +3,9 @@ class foo
 {
 public static void main(String ar[])throws Exception
 {
+//this is a comment
+int var=10;
+var--;
 System.out.println("hello world foo.!!");
 }
 }
---
+++ foo2.java
@@ -0,0 +1,2 @@
+this is foo2 for Test repo
+hope this clearify things 
